1e1399ad39f8c18d46ad335b96dd417c2ce7321b
windmikkun <windmikkun@gmail.com>
2025-06-17 06:56:04 +0900

    pg practice reset and revert

---
+++ pg_practice.sql
@@ -0,0 +1,7 @@
+-- 問題1. postgresqlでpracticeデータベースを生成するSQL文を記述してください。
+
+-- 問題2. postgresqlでpracticeデータベースのusersテーブルを生成するSQL文を記述してください。
+
+-- 問題3. postgresqlでpracticeデータベースのjobsテーブルを生成するSQL文を記述してください。
+
+-- CREATE TABLE 'テーブル名'の後にカラム必須です。
