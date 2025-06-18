12128fd82f0caa9a0539c4ffd3dcc1a5176db46f
windmikkun <windmikkun@gmail.com>
2025-06-18 11:22:07 +0900

    MySQL→postgreSQL

--- pg_practice.sql
+++ pg_practice.sql
@@ -1,23 +1,27 @@
 -- 問題1. postgresqlでpracticeデータベースを生成するSQL文を記述してください。
 CREATE DATABASE practice;
 -- 問題2. postgresqlでpracticeデータベースのusersテーブルを生成するSQL文を記述してください。
--- practiceDBに移動
-use practice;
+-- useではなく \cを使用する
+\c practice;
 --テーブル作成
-CREATE TABLE `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` char(255) NOT NULL DEFAULT '' Comment '氏名',
-  `age` int NOT NULL DEFAULT 0 Comment '年齢',
-  `gender` enum('Man','Woman','Other') NOT NULL DEFAULT 'Other' Comment '性別',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- 問題3. postgresqlでpracticeデータベースのjobsテーブルを生成するSQL文を記述してください。
-CREATE TABLE `jobs` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
-  `name` char(255) NOT NULL DEFAULT '会社員' Comment '仕事名',
-  PRIMARY KEY (`id`),
-  FOREIGN KEY `fk_user_id` (`user_id`) REFERENCES `users`(`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL DEFAULT '',
+  age INTEGER NOT NULL DEFAULT 0,
+  gender VARCHAR(10) NOT NULL DEFAULT 'Other' CHECK (gender IN ('Man','Woman','Other'))
+);
+COMMENT ON COLUMN users.name IS '氏名';
+COMMENT ON COLUMN users.age IS '年齢';
+COMMENT ON COLUMN users.gender IS '性別';
 
+-- 問題3. postgresqlでpracticeデータベースのjobsテーブルを生成するSQL文を記述してください。
+CREATE TABLE jobs (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL REFERENCES users(id),
+  name VARCHAR(255) NOT NULL DEFAULT '会社員'
+);
+
+COMMENT ON COLUMN jobs.name IS '仕事名';
+COMMENT ON COLUMN jobs.user_id IS 'ユーザーID';
 -- CREATE TABLE 'テーブル名'の後にカラムを入力しなかったらエラー。
+--postgreSQLはMySQLと違ってコメントは別で行う必要がある。
